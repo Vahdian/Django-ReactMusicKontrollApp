6490c5730521c2cca211c1e04ef69a48919661dc
vahdian <germanpalero@gmail.com>
2021-01-31 20:09:52 +0100

    Second step create room is kinda done

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import {render} from "react-dom";
+import HomePage from "./HomePage"; 
+
 
 export default class App extends Component {
     constructor(props){
@@ -7,9 +9,11 @@ export default class App extends Component {
 
     }
     render(){
-        return <h1>{this.props.name}</h1>;
+        return (<div>
+        <HomePage/>
+        </div>);
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name="Ger"/>, appDiv);
+render(<App/>, appDiv);
--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('join', index),
+    path('create', index)
   
 ]
